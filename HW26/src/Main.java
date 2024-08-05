3265184af9857579e54280b1a901f49166ee6ed0
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-08-05 20:45:55 +0300

    HW26 completed

--- HW26/src/Main.java
+++ HW26/src/Main.java
@@ -1,6 +1,5 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,10 +27,81 @@ public class Main {
         sets.add(strings3);
         sets.add(strings4);
 
-        //todo Решение дз 1 тут
+        System.out.println("Общее количество букв в коллекции: " +
+                sets.stream()
+                        .flatMap(s->s.stream())
+                        .mapToInt(s->s.length())
+                        .sum());
 
-        //Task2
+        System.out.println("Длина самого большого слова в коллекции: " +
+                sets.stream()
+                        .flatMap(s->s.stream())
+                        .mapToInt(s->s.length())
+                        .max()
+                        .getAsInt());
+
+
         List<Employee> employees = EmployeeFactory.createEmployee();
-        //todo решения дз 2 тут
+
+        Employee maxKpiEmployee = employees.stream()
+                .max(Comparator.comparing(Employee::getKpi))
+                .get();
+        System.out.println("\nРаботника с самым большим KPI зовут "
+                + maxKpiEmployee.getName() + " "
+                + maxKpiEmployee.getSurname()
+        );
+
+        System.out.println("\nСотрудники, которым пора на пенсию: ");
+        employees.stream()
+                .filter(e->e.getAge()>65)
+                .forEach(System.out::println);
+
+
+        System.out.println("\nСотрудник с самой высокой зарплатой:\n" +
+                        employees.stream()
+                                .max(Comparator.comparing(Employee::getSalary))
+                                .get());
+
+        System.out.println("\nИностранные сотрудники");
+        employees.stream()
+                .filter(e->e.getName()
+                .matches("[A-z]+"))
+                .forEach(System.out::println);
+
+        System.out.println("\nСписок сотрудников с kpi выше среднего");
+        employees.stream()
+                .filter(e-> e.getKpi() >
+                        (employees.stream()
+                                .mapToInt(Employee::getKpi)
+                                .average())
+                                .getAsDouble())
+                .forEach(System.out::println);
+
+        System.out.println("\nЗначение среднего kpi у русских работников, " +
+                "которым меньше 45 лет и зп которых больше 20000");
+        double avgKPI = employees.stream()
+                .filter(n->n.getName().matches("[А-яЁё]+"))
+                .filter(e->e.getAge() < 45)
+                .filter(emp->emp.getSalary()>20000)
+                .mapToInt(Employee::getKpi)
+                .average()
+                .orElse(0);
+        System.out.println(avgKPI);
+
+       //Решение задания 4
+        Map<String, Employee> namesToEmployees = new HashMap<>();
+
+        employees.stream()
+                .filter(e->e.getAge()<35 && e.getSalary()>10000)
+                .forEach(e->namesToEmployees.put((e.getSurname() + " " + e.getName()), e));
+
+        //Альтернативное решение задания 4
+        Map<String,Employee> namesToEmployees1 = employees.stream()
+                .filter(e->e.getAge()<35 && e.getSalary()>10000)
+                .collect(Collectors.toMap(e->e.getSurname() + " " + e.getName(), e->e));
+
+
+        System.out.println("\nСписок имена-сотрудники из задания 4: ");
+        namesToEmployees.entrySet().forEach(e->System.out.println(e.getKey() + " " + e.getValue()));
     }
 }
